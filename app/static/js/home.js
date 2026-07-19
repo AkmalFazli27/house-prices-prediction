6e42fa5692f100323460ef2f9cc1e03403160710
Xiamengs <akmalfazli27@gmail.com>
2026-07-19 17:11:20 +0700

    fix(home): wrap IntersectionObserver setup in an IIFE for better scope management

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,15 +1,19 @@
 // animate number when scroll to stats-section
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        animateStats();
-        observer.disconnect();
-      }
-    });
-  },
-  { threshold: 0.5 },
-);
+(function () {
+  const stats = document.querySelector(".stats-section");
+  if (!stats) return;
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          animateStats();
+          observer.disconnect();
+        }
+      });
+    },
+    { threshold: 0.5 },
+  );
+});
 
 const statsSection = document.querySelector(".stats-section");
 if (statsSection) observer.observe(statsSection);
